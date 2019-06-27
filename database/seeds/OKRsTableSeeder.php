a8fc753b18a037418db210d0b15984daec5cb0bd
Pablo <prosa85@yahoo.com>
2019-06-27 10:56:51 -0400

    platform instructions

--- database/seeds/OKRsTableSeeder.php
+++ database/seeds/OKRsTableSeeder.php
@@ -13,6 +13,16 @@ class OKRsTableSeeder extends Seeder
     public function run()
     {
         $okrs[] = [
+            "created_by" => 4,
+            "OKRs_title" => "Provide a platform to manage OKRs, KR, Tasks and reports",
+            "description" => "the expanded button shows the descriptions off okr, krs, and tasks. The statuses are inherited from the children on every update. example. an OKR with 1 kr that has 3 completed task and 1 active. OKR->Active KR->Active tasks->active 1 completed 3. when the last task gets completed the OKR and KR will change to Completed too.",
+            "status" => "Proposed",
+            "categories" => ["#Q2C", "#P&S", "#BO"],
+            "contributors" => ["@BS", "@SW", "@QA", "@ARCH"],
+            "impact_groups" => ["PAR", "CUS", "EMP"],
+            "target_date" => "2019-12-20"
+        ];
+        $okrs[] = [
             "created_by" => 1,
             "OKRs_title" => "Deliver a unified on-line experience for customers, partners and employees to gain access to the primary tools and information they require, improving service and reducing costs.",
             "description" => "This involves merging the capabilities of in\site, vX Tools, vX Price and vXacademy into a single UX/UI experience for the end user accessible via one username and password and controlled by a unified role and permissions framework.",
