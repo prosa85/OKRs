a7220a9e077c517a9bacbd97e253b80671c8d1c6
Prosa <prosa@vxchnge.com>
2019-12-06 17:15:26 -0500

    adding data to the seeder.

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,9 +17,10 @@ class UsersTableSeeder extends Seeder
 //        $users['amarashi'] = [ 'first_name' => "Ali", 'last_name' => "Marashi", 'supervisor_id'=>0 ,'title' => "CTO", 'email' => "aseal@vxchnge.com", 'role' => 1, 'department' => 1, 'password' => bcrypt("test"), 'api_token' => Str::random(60)] ;
 //        $users['aseal'] = [ 'first_name' => "Alan", 'last_name' => "Seal", 'supervisor_id'=>1 ,'title' => "VP", 'email' => "amarashi@vxchnge.com", 'role' => 1, 'department' => 1, 'password' => bcrypt("test"), 'api_token' => Str::random(60)] ;
 //        $users['famoon'] = [ 'first_name' => "Foad", 'last_name' => "Amoon", 'supervisor_id'=>1 ,'title' => "Director", 'email' => "famoon@vxchnge.com", 'role' => 1, 'department' => 1, 'password' => bcrypt("test"), 'api_token' => Str::random(60)] ;
-        $users['prosaTest'] = [ 'first_name' => "Pablo", 'last_name' => "Rosa", 'supervisor_id'=>0 ,'title' => "Developer", 'email' => "prosa@yahoo.com", 'role' => 1, 'department' => 1, 'password' => bcrypt("test"), 'api_token' => Str::random(60),] ;
+        $users['prosaAdmin'] = [ 'first_name' => "PabloAdmin", 'last_name' => "RosaAdmin", 'supervisor_id'=>0 ,'title' => "Dev", 'email' => "prosa85@yahoo.com", 'role' => 1, 'department' => 1, 'password' => bcrypt("test"), 'api_token' => Str::random(60),] ;
         $users['prosa'] = [ 'first_name' => "Pablo", 'last_name' => "Rosa", 'supervisor_id'=>1 ,'title' => "Developer", 'email' => "prosa@vxchnge.com", 'role' => 1, 'department' => 1, 'password' => bcrypt("test"), 'api_token' => Str::random(60),] ;
-        $users['mike'] = [ 'first_name' => "Mike", 'last_name' => "Burn", 'supervisor_id'=>1 ,'title' => "Developer", 'email' => "prosa@vxchnge.com", 'role' => 1, 'department' => 1, 'password' => bcrypt("test"), 'api_token' => Str::random(60),] ;
+        $users['mike'] = [ 'first_name' => "Mike", 'last_name' => "Burn", 'supervisor_id'=>1 ,'title' => "Developer", 'email' => "mike@immertec.com", 'role' => 1, 'department' => 1, 'password' => bcrypt("test"), 'api_token' => Str::random(60),] ;
+        $users['john'] = [ 'first_name' => "Joh", 'last_name' => "Test", 'supervisor_id'=>1 ,'title' => "CTO", 'email' => "Jon@Immertec.com", 'role' => 1, 'department' => 1, 'password' => bcrypt("test"), 'api_token' => Str::random(60),] ;
 
 
           foreach($users as $user){
